65bc59d2c896a2793c2a4606cc17e3c507735ce4
cid <cid@ciddebijibendiannao.local>
2025-12-31 14:16:09 +0800

    log

--- sources/knowledge/knowledge.py
+++ sources/knowledge/knowledge.py
@@ -179,7 +179,7 @@ def get_redis_connection():
     # 优先从环境变量获取 Redis 配置
     redis_host = os.getenv('REDIS_HOST')
     redis_port = os.getenv('REDIS_PORT')
-
+    logger.info(f"redis_host: {redis_host}, redis_port: {redis_port}")
     return redis.Redis(host=redis_host, port=redis_port, decode_responses=True)
 
 
